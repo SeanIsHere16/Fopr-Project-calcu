f6639fa7f717b735122f2fcfd3c491d30e067690
SeanIsHere16 <sean03765@gmail.com>
2025-04-09 10:20:51 +0800

    Add files via upload

---
+++ calculator fopr.cpp
@@ -0,0 +1,78 @@
+#include<stdio.h>
+
+int myCalc() {
+    int nums[3];
+    char op;
+    int ans = 0, i;
+
+    printf("\n=======================================================================\n");
+    printf("\nEnter Three numbers\n");
+    for (i = 0; i < 3; i++) {
+        printf("Input Number %d: ", i + 1);
+        scanf("%d", &nums[i]);
+    }
+
+    while (getchar() != '\n');
+
+    printf("Enter an operator (+, -, *, /, %%, i for increment, d for decrement): ");
+    scanf(" %c", &op);
+
+    switch (op) {
+        case '+':
+            ans = nums[0] + nums[1] + nums[2];
+            printf("\nThe result of %d, %d, and %d is %d\n", nums[0], nums[1], nums[2], ans);
+            break;
+        case '-':
+            ans = nums[0] - nums[1] - nums[2];
+            printf("\nThe result of %d, %d, and %d is %d\n", nums[0], nums[1], nums[2], ans);
+            break;
+        case '*':
+            ans = nums[0] * nums[1] * nums[2];
+            printf("\nThe result of %d, %d, and %d is %d\n", nums[0], nums[1], nums[2], ans);
+            break;
+        case '/':
+            if (nums[1] != 0 && nums[2] != 0) {
+                ans = nums[0] / nums[1] / nums[2];
+                printf("\nThe result of %d, %d, and %d is %d\n", nums[0], nums[1], nums[2], ans);
+            } else {
+                printf("\nInvalid Divisor\n");
+            }
+            break;
+        case '%':
+            if (nums[1] != 0 && nums[2] != 0) {
+                ans = nums[0] % nums[1] % nums[2];
+                printf("\nThe result of %d, %d, and %d is %d\n", nums[0], nums[1], nums[2], ans);
+            } else {
+                printf("\nInvalid Divisor\n");
+            }
+            break;
+        case 'i':
+            printf("\nThe result of %d, %d, and %d\n", nums[0] + nums[1], nums[1] + nums[2], nums[2] + nums[0]);
+            break;
+        case 'd':
+            printf("\nThe result of %d, %d, and %d\n", nums[0] - nums[1], nums[1] - nums[2], nums[2] - nums[0]);
+            break;
+        default:
+            printf("\nInvalid operator\n");
+    }
+
+    return 0; // Return 0 to indicate success
+}
+
+int main() {
+    char tryUle;
+
+    do {
+     
+        myCalc();
+
+        printf("\nDo you want to try again (y/n): ");
+        while (getchar() != '\n');
+        scanf("%c", &tryUle);
+
+    } while (tryUle == 'y' || tryUle == 'Y');
+
+    printf("\nThank you for using the calculator.\n");
+
+    return 0; // Main also returns 0 to indicate successful execution
+}
